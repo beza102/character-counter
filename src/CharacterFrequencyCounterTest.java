7ff4457efc29da349963ed856ef1d7ee30613def
VibeSyntax <138536097+beza102@users.noreply.github.com>
2025-01-20 21:59:39 -0800

    Finished character counter assignment

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,27 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    public void testLAppearsThreeTimesInHelloWorld(){
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello world");
+
+        //act
+        int actualCount = counter.getFrequency('l');
+        //assert
+        assertEquals(3, actualCount);
+
+
+    }
+    @Test
+    public void testNoneOccuringCharachterPercentagezero(){
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hi there");
+
+        //act
+        double actualPercentage = counter.getRelativePercentage('z');
+
+        //assert
+        assertEquals(0.0, actualPercentage, 0.0001);
+    }
 }
